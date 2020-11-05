725342f51aa28ee2527f0a381be310c766b5db4f
mlarcher531 <mlarcher531@gmail.com>
2020-11-05 17:43:30 -0600

    user and orders routes

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::UsersController < ApplicationController
-    # def index
-    #     users = User.all
-    #     render json: users, only: [:id, :username, :password]
-    # end
+  def index
+    users = User.all
+    render json: users, only: [:id, :username, :password, :address, :email]
+  end
 
-    # def show
-    #     user = User.find(params[:id])
-    #     render json: user, only: [:id, :username, :password]
-    # end
+  def show
+    user = User.find(params[:id])
+    render json: user, only: [:id, :username, :password, :address, :email]
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :products
+      get("/products", to: "products#index")
+      get("products/:id", to: "products#show")
 
-    namespace :api do 
-        namespace :v1 do 
-            resources :products
-            get('/products', to: 'products#index')
-            get('products/:id', to: 'products#show')
-        end
+      get("/orders", to: "orders#index")
+      get("orders/:id", to: "orders#show")
+
+      get("/users", to: "users#index")
+      get("users/:id", to: "users#show")
     end
+  end
 end
