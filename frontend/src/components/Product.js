3067fe51276da00749de338c50ed6b66047cb940
github username <github email address>
2020-11-09 09:44:24 -0600

    styled product page

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,17 +5,15 @@ import { Card } from 'react-bootstrap'
 class Product extends React.Component {
   render() {
     return (
-      <Card className='m-3 p-3 rounded '>
+      <Card className='my-3 p-3 rounded '>
         <Link to={`products/${this.props.product.id}`}>
-          <Card.Img src={this.props.product.image_url} variant='top' fluid />
+          <Card.Img src={this.props.product.image_url} variant='top' />
         </Link>
         <Card.Body>
           <Link to={`products/${this.props.product.id}`}>
             <Card.Title as='div'>{this.props.product.name}</Card.Title>
           </Link>
-          <Card.Text as='div'>
-            <h3>${this.props.product.price}</h3>
-          </Card.Text>
+          <Card.Text as='h3'>${this.props.product.price}</Card.Text>
         </Card.Body>
       </Card>
     )
